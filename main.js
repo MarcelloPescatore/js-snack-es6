7b8c89112b055fe39e828e5b9798967544d228a7
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-11 16:16:42 +0200

    compleate first snack

--- main.js
+++ main.js
@@ -6,8 +6,128 @@
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
 
+// create an array of objects
+const bikeShop = [
+    {
+        name: 'pinco',
+        weight: 80,
+    },
+    {
+        name: 'pallo',
+        weight: 60,
+    },
+    {
+        name: 'panco',
+        weight: 25,
+    },
+    {
+        name: 'caio',
+        weight: 90,
+    },
+    {
+        name: 'zefiro',
+        weight: 78,
+    },
+    {
+        name: 'tornado',
+        weight: 45,
+    },
+    {
+        name: 'fulmine',
+        weight: 32,
+    },
+    {
+        name: 'saetta',
+        weight: 68,
+    },
+    {
+        name: 'vento',
+        weight: 56,
+    },
+    {
+        name: 'ombra',
+        weight: 72,
+    },
+    {
+        name: 'falco',
+        weight: 38,
+    },
+    {
+        name: 'dragone',
+        weight: 83,
+    },
+    {
+        name: 'fenice',
+        weight: 41,
+    },
+    {
+        name: 'griffone',
+        weight: 50,
+    },
+    {
+        name: 'levante',
+        weight: 29,
+    },
+    {
+        name: 'siluro',
+        weight: 62,
+    },
+    {
+        name: 'avvoltoio',
+        weight: 95,
+    },
+    {
+        name: 'lampo',
+        weight: 36,
+    },
+    {
+        name: 'tuono',
+        weight: 70,
+    },
+    {
+        name: 'zephyr',
+        weight: 52,
+    },
+    {
+        name: 'scorpione',
+        weight: 46,
+    },
+    {
+        name: 'pegaso',
+        weight: 82,
+    },
+    {
+        name: 'sirio',
+        weight: 34,
+    },
+    {
+        name: 'eolo',
+        weight: 76,
+    }
+];
 
 
+// create a loop take all the weight and then find the smallest number
+
+// create an array with all weight
+let allWeight = [];
+// create a loop
+for (let i = 0; i< bikeShop.length; i++){
+    let singleWeight = bikeShop[i].weight;
+    allWeight.push(singleWeight);
+}
+
+// find a smallest weight
+let smallestWeight = Math.min(...allWeight); //spread operator
+console.log(smallestWeight);
+for (let i = 0; i< bikeShop.length; i++){
+    if (bikeShop[i].weight === smallestWeight){
+        const lightestBike = bikeShop[i]
+        
+        document.querySelector('.name').innerHTML = `${bikeShop[i].name}`
+        document.querySelector('.weight').innerHTML = `Peso: ${bikeShop[i].weight}kg`
+    }
+}
 
 
 
